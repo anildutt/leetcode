dd361016ef508df0f56618ce3cb278ce368c4a42
Anil Dutt Yemmanuru <anildatt@gmail.com>
2018-01-13 17:56:44 -0800

    208	 Implement Trie (Prefix Tree)
    208 Implement Trie (Prefix Tree)    
    
    Implement a trie with insert, search, and startsWith methods.
    
    Note:
    You may assume that all inputs are consist of lowercase letters a-z.

---
+++ Trie.java
@@ -0,0 +1,69 @@
+class Trie {
+    
+    TrieNode root;
+    class TrieNode{
+        char ch;
+        Map<Character, TrieNode> children;
+        boolean isLeaf;
+        
+        public TrieNode(){
+            children = new HashMap<Character, TrieNode>();
+        }
+        public TrieNode(char c){
+            ch = c;
+            children = new HashMap<Character, TrieNode>();
+        }
+    }
+    
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(int i=0; i<word.length(); i++){
+            char curc = word.charAt(i);
+            if(cur.children.containsKey(curc))
+                cur = cur.children.get(curc);
+            else{
+                TrieNode child =  new TrieNode(curc);
+                cur.children.put(curc, child);
+                cur = child;
+            }
+        }
+        cur.isLeaf = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        int i = 0;
+        TrieNode cur = root;
+        
+        while(i<word.length()){
+            if(cur.children.containsKey(word.charAt(i))){
+                cur = cur.children.get(word.charAt(i));
+            }else
+                return false;
+            i++;
+        }
+        
+        return cur.isLeaf;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String word) {
+        int i = 0;
+        TrieNode cur = root;
+        while(i<word.length()){
+            if(cur.children.containsKey(word.charAt(i))){
+                cur = cur.children.get(word.charAt(i));
+            }else
+                return false;
+            i++;
+        }
+        
+        return true;
+    }
+}
