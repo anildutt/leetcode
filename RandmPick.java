bec02faefc8064d5c178e657657d7d0d228bf329
Anil Dutt Yemmanuru <anildatt@gmail.com>
2018-01-20 09:54:04 -0800

    398. Random Pick Index
    398. Random Pick Index

---
+++ RandmPick.java
@@ -0,0 +1,50 @@
+class Solution {
+    Map<Integer, List<Integer>> map;
+    int[] nums;
+    
+    public Solution(int[] nums) {
+        //approach 1
+        /* 
+        map = new HashMap<Integer, List<Integer>>();
+        for(int i = 0; i<nums.length; i++){
+            int num = nums[i];
+            if(map.containsKey(num))
+                map.get(num).add(i);
+            else{
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                map.put(num, list);
+            }
+        } */
+        
+        //aproach 2
+        this.nums = nums;
+    }
+    
+    public int pick(int target) {
+        //approach 1 - using Random directly on indices
+        /*
+        List<Integer> indices = map.get(target);
+        Random rand = new Random();
+        int randIdx = rand.nextInt(indices.size());
+        return indices.get(randIdx); */
+        
+        //approach 2 - Rervoir Sampling
+        Random rnd = new Random();
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                if (rnd.nextInt(++count) == 0)
+                    result = i;
+        }
+
+        return result;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
