6e345088caa08b78bb2ce8ca2654ed9a53adb537
Anil Dutt Yemmanuru <anildatt@gmail.com>
2018-01-27 07:38:04 -0800

    46. Permutations
    46. Permutations of an array of integers

---
+++ Permutations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        recur(nums, result, 0);
+        return result;
+    }
+    
+    private void recur(int[] nums, List<List<Integer>> result, int index){
+        if(index == nums.length){
+            List<Integer> sol = new ArrayList<Integer>();
+            for(int i=0; i<nums.length; i++)
+                sol.add(nums[i]);
+            result.add(sol);
+        }
+        
+        for(int i=index; i<nums.length; i++){
+            int tmp = nums[i];
+            nums[i] = nums[index];
+            nums[index] = tmp;
+            
+            recur(nums, result, index+1);
+            
+            tmp = nums[i];
+            nums[i] = nums[index];
+            nums[index] = tmp;
+        }
+        
+    }
+}
