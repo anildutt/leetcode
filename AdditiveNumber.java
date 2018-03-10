f27f6f0f3f65d0ac74df74f008fc0798b158937e
Anil Dutt Yemmanuru <anildatt@gmail.com>
2018-03-10 15:48:32 -0800

    306. Additive Number
    Additive number is a string whose digits can form additive sequence.
    
    A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
    
    For example:
    "112358" is an additive number because the digits can form an additive sequence: 1, 1, 2, 3, 5, 8.
    
    1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
    "199100199" is also an additive number, the additive sequence is: 1, 99, 100, 199.
    1 + 99 = 100, 99 + 100 = 199
    Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
    
    Given a string containing only digits '0'-'9', write a function to determine if it's an additive number.
    
    Follow up:
    How would you handle overflow for very large input integers?

---
+++ AdditiveNumber.java
@@ -0,0 +1,30 @@
+class Solution {
+	public boolean isAdditiveNumber(String num) {
+		for (int i = 0; i < num.length() / 2; i++) {
+			for (int j = i + 1; j < num.length() - 1; j++) {
+				String num1 = num.substring(0,i+1);
+				String num2 = num.substring(i + 1, j + 1);
+				String rest = num.substring(j + 1);
+				
+                if(isInvalid(num1) || isInvalid(num2) || isInvalid(rest))
+				    continue;
+				if (isAdditive(num1, num2, rest))
+					return true;
+			}
+		}
+
+		return false;
+	}
+    
+    private boolean isInvalid(String num){  return num.length() > 1 && num.startsWith("0"); }
+	private boolean isAdditive(String num1, String num2, String num) {
+		long n1 = Long.valueOf(num1);
+		long n2 = Long.valueOf(num2);
+		String addition = String.valueOf(n1 + n2);
+		if(num.equals(addition)) return true;
+		if (num.startsWith(addition) == false)
+			return false;
+		else
+			return isAdditive(num2, addition, num.substring(addition.length()));
+	}
+}
