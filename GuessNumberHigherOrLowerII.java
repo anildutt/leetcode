d298189a2cea2ebf5f6765008ff6ef8c4b671be5
Anil Dutt Yemmanuru <anildatt@gmail.com>
2018-03-18 16:26:41 -0700

    375. Guess Number Higher or Lower II
    We are playing the Guess Game. The game is as follows:
    
    I pick a number from 1 to n. You have to guess which number I picked.
    
    Every time you guess wrong, I'll tell you whether the number I picked is higher or lower.
    
    However, when you guess a particular number x, and you guess wrong, you pay $x. You win the game when you guess the number I picked.
    
    Example:
    
    n = 10, I pick 8.
    
    First round:  You guess 5, I tell you that it's higher. You pay $5.
    Second round: You guess 7, I tell you that it's higher. You pay $7.
    Third round:  You guess 9, I tell you that it's lower. You pay $9.
    
    Game over. 8 is the number I picked.
    
    You end up paying $5 + $7 + $9 = $21.
    Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.

---
+++ GuessNumberHigherOrLowerII.java
@@ -0,0 +1,31 @@
+/**
+ Algo - DP[i][j] = Minimum { x$ + Max{ DP[i-1 -> x], DP[x+1 -> j]}  
+                    over all x ranging from i -> j;
+        answer = DP[0][n-1];
+        DP[i][i] = 0; //only one element => 1st guess is right => cost = 0;
+        DP[i][i+1] = i; //only 2 elements => 1st guess the minimum number i, in the worst case wrong i+1 is right.
+**/
+
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] DP = new int[n][n];
+        
+        //Dp[i,i] = 0; for i=0 -> n-1;
+        //Dp[i,i+1] = i;
+        for(int i=0; i<n-1; i++)
+            DP[i][i+1] = i+1;
+        
+        for(int gap=2; gap<=n-1; gap++){
+            for(int start=0; start+gap<n; start++){
+                int end = start+gap;
+                int globalMin = Integer.MAX_VALUE;
+                for(int i=start+1; i<=end-1; i++){
+                    globalMin = Math.min(i + 1 + Math.max(DP[start][i-1], DP[i+1][end]), globalMin);
+                }
+                DP[start][end] = globalMin;
+            }
+        }
+        
+        return DP[0][n-1];
+    }
+}
